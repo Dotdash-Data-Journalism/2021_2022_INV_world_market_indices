4fa7fb073428f2accf749c0d98cd532b743e3755
anesta95 <adriannesta@gmail.com>
2022-03-18 12:20:28 -0400

    Adding python and yml files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Investopedia World Market Indices
+
+This repository calculates the daily percent change in price of these 13 global financial indices:
+Australian All, S&P BSE SENSEX, NIKKEI 225, Korea KOSPI, Shenzhen Component, SSE Shanghai Composite, Hang Seng, CAC 40, Deutsche Boerse DAX, FTSE 100, Tel Aviv 125, S&P/TSX Composite, S&P 500.
+
+Data is gathered every hour from `yfinance` the [Python wrapper](https://pypi.org/project/yfinance/) for the [Yahoo Finance API](https://blog.api.rakuten.net/api-tutorial-yahoo-finance/). Data is then compiled into the CSV file `worldmarketIndices.csv` and used for a [Datawrapper](https://app.datawrapper.de) visualization in the [Investopedia](https://investopedia.com) daily newsletter. The data in the CSV file is updated hourly between the hours of 6AM & 11AM EST.
---
+++ worldMarketIndicesUpdate.py
@@ -0,0 +1,123 @@
+import yfinance as yf
+import time
+import pandas as pd
+import os
+import requests
+from datetime import datetime
+import pytz
+
+# Function used to add new data to datawrapper chart via a pandas dataframe and 
+# places the latest update date in the chart notes
+def updateChart(dw_chart_id, dataSet, updateTitle, updateDate, dw_api_key):
+
+    headers = {
+    "Accept": "*/*",
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {dw_api_key}"
+    }
+
+    stringDataSet = dataSet.to_csv(path_or_buf=None, index=False, header=True)   
+
+    dataRefresh = requests.put(url=f"https://api.datawrapper.de/v3/charts/{dw_chart_id}/data", 
+    data=stringDataSet,
+    headers={"Authorization": f"Bearer {dw_api_key}"}
+    )
+
+    dataRefresh.raise_for_status()
+
+    time.sleep(2)
+
+    callBack = {"title": updateTitle,
+                "metadata": {
+                    "annotate": {
+                        "notes": f"Data from Yahoo Finance. Updated {updateDate}"
+                    }
+                }
+            }
+    notesRes = requests.patch(url=f"https://api.datawrapper.de/v3/charts/{dw_chart_id}",
+    json=callBack,
+    headers=headers)
+
+    notesRes.raise_for_status()
+
+    time.sleep(2)
+
+    publishRes = requests.post(url=f"https://api.datawrapper.de/v3/charts/{dw_chart_id}/publish",
+    headers=headers)
+
+    publishRes.raise_for_status()
+
+
+ACCESS_TOKEN = os.getenv('DW_API_KEY')
+
+wm = yf.Tickers("^AORD ^BSESN ^N225 ^KS11 399001.SZ 000001.SS ^HSI ^FCHI ^GDAXI ^FTSE ^TA125.TA ^GSPTSE ^GSPC")
+
+pctCngList = []
+
+for key, value in wm.tickers.items():
+    time.sleep(1)
+    stock = value.info
+    pctCng = str(round(((stock['regularMarketPrice'] - stock['previousClose']) / stock['previousClose']) * 100, 2)) + '%'
+    pctCngList.append(pctCng)
+
+vmDict = {'Index': ['Australian All :au:',
+                    'S&P BSE SENSEX :in:',
+                    'NIKKEI 225 :jp:',
+                    'Korea KOSPI :kr:',
+                    'Shenzhen Component :cn:',
+                    'SSE Shanghai Composite :cn:',
+                    'Hang Seng :hk:',
+                    'CAC 40 :fr:',
+                    'Deutsche Boerse DAX :de:',
+                    'FTSE 100 :gb:',
+                    'Tel Aviv 125 :il:',
+                    'S&P/TSX Composite :ca:',
+                    'S&P 500 :us:'
+], 'Change %': pctCngList}
+
+wmDF = pd.DataFrame(data=vmDict)
+
+wmDF.to_csv("worldMarketIndices.csv", index=False)
+
+time.sleep(2)
+
+EST = pytz.timezone('US/Eastern')
+rightnow = datetime.now(EST)
+rightnowformated = rightnow.strftime('%H:%M %p %Z')
+fileDate = str(datetime.today().strftime('%B %d, %Y'))
+chartTitle = f"World Market Indices at {str(rightnowformated)}"
+
+
+updateChart("AVLCt", wmDF, chartTitle, fileDate, ACCESS_TOKEN)
+
+# wfCSV = wmDF.to_csv(index=False)
+
+# url = 'https://api.datawrapper.de/v3/charts/AVLCt/data'
+
+# headers = {
+#     'authorization':f'{DW_API}',
+#     'content-type': 'text/csv'
+# }
+# r = requests.put(url, data=wfCSV, headers=headers)
+
+# r.raise_for_status()
+
+# urltwo = 'https://api.datawrapper.de/v3/charts/AVLCt'
+
+# headerstwo = {
+#     'authorization':f'{DW_API}',
+#     'content-type': 'application/json'
+# }
+
+# EST = pytz.timezone('US/Eastern')
+# rightnow = datetime.now(EST)
+# rightnowformated = rightnow.strftime('%H:%M %p')
+# rtwo = requests.patch(urltwo, data={'title': f'World Market Indices at {rightnowformated}'}, headers=headerstwo)
+
+# rtwo.raise_for_status()
+
+# urlthree = "https://api.datawrapper.de/v3/charts/AVLCt/publish"
+
+# headersthree = {'authorization':f'{DW_API}'}
+# rthree = requests.post(urlthree, headers=headersthree)
+# rthree.raise_for_status()
